866743515558d47905fce783f4f6102f643604d5
TruccoloRenan <renan4040@gmail.com>
2024-02-19 20:08:18 -0300

    fix done ready for next version

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -42,17 +42,23 @@ const About = () => {
         variants={fadeIn("", ", 0.1, 1")}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        Hello! 🌟
+        Hello! 🌟 <br />
 
-        I am an enthusiast in the world of technology, a Full Stack Developer with a vibrant passion for creating and innovating in back-end and front-end. My journey with Node.js and React has been an adventure in process optimization and creating software solutions that not only work well, but also delight. I love the energy of dynamic environments where I can bring new ideas to life!
+        I am an enthusiast in the world of technology, a Full Stack Developer with a vibrant passion for creating and innovating in back-end and front-end.<br />
+        My journey with Node.js and React has been an adventure in process optimization and creating software solutions that not only work well, but also delight.<br />
+        I love the energy of dynamic environments where I can bring new ideas to life!<br />
 
-        My training is a mix of theory and practice: Software Engineering and Systems Analysis and Development. Oh, and my fluent English opens doors for me to global projects and multicultural teams.
+        My training is a mix of theory and practice: Software Engineering and Systems Analysis and Development.<br />
+        Oh, and my fluent English opens doors for me to global projects and multicultural teams.<br />
 
-        My professional experience is a colorful and impressive patchwork: from improving front-ends at Fretefy to rebuilding systems at Tech Mahindra, each project was a new learning experience. At MatrixCargo, SóCarrão.com and ApoioEcolimp, I dove headfirst into software development, playing with Angular, React, Next.js and Node.js to create something special.
+        My professional experience is a colorful and impressive patchwork: from improving front-ends at Fretefy to rebuilding systems at Tech Mahindra, each project was a new learning experience.<br />
+        At MatrixCargo, SóCarrão.com and ApoioEcolimp, I dove headfirst into software development, playing with Angular, React, Next.js and Node.js to create something special.<br />
 
-        Speaking of technical skills, I like to think of myself as a complete 'toolbox': CSS, HTML, TypeScript, Next.js, Bootstrap, React, JavaScript, Angular, SCSS, VueJS on the front end; and C#, Python, Node.js on the back-end, without forgetting the SQL and Repository databases. I'm always learning and improving myself, like Dani Code's Full Stack Master Package.
+        Speaking of technical skills, I like to think of myself as a complete 'toolbox': CSS, HTML, TypeScript, Next.js, Bootstrap, React, JavaScript, Angular, SCSS, VueJS on the front end; and C#, Python, Node.js on the back-end, without forgetting the SQL and Repository databases.<br />
+        I'm always learning and improving myself, like Dani Code's Full Stack Master Package.<br />
 
-        I'm super excited to connect with professionals and companies looking for a developer full of ideas and readiness to transform challenges into opportunities. Let's navigate the exciting world of Full Stack development together and make a difference with innovative software solutions! Are you coming with me on this technological journey? 👨‍💻✨
+        I'm super excited to connect with professionals and companies looking for a developer full of ideas and readiness to transform challenges into opportunities.<br />
+        Let's navigate the exciting world of Full Stack development together and make a difference with innovative software solutions! Are you coming with me on this technological journey? 👨‍💻✨<br />
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
